bde13cc31327c4f249505953f861c08a6ca437c8
Lucas Nascimento <4311885+hexetia@users.noreply.github.com>
2021-06-10 22:23:11 -0300

    chore: update github link

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -1,7 +1,7 @@
 export function PageFooter() {
   return (
     <footer className='mt-20 mb-5 flex justify-between md:flex-row max-w-sm mx-auto border-t-1 border-white'>
-      <a className='p-2 ' href='https://github.com/morhogg' target='_blank'>
+      <a className='p-2 ' href='https://github.com/hexetia' target='_blank'>
         Github
       </a>
       <a className='p-2' href='https://www.linkedin.com/in/lucasdev7/' target='_blank'>

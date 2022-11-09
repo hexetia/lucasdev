0680236e9b2976f6e0fe9a0b308b052ba08aaa37
Lucas Nascimento <4311885+hexetia@users.noreply.github.com>
2022-11-09 16:33:46 -0300

    update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
                   impressão em gráficas de dezenas de milhares de estudantes por ano em Pernambuco.
                 </p>
                 <p>
-                  Meu objetivo principal foi prover uma aplicação flexível para que novas entidades estudantis, com outros formatos e
+                  Parte do objetivo foi também prover uma aplicação flexível para que novas entidades estudantis que participaram da construção, com outros formatos e
                   requisitos pudessem ser adicionadas posteriormente, sem comprometer a segurança e performance.
                 </p>
               </div>
@@ -43,6 +43,7 @@ export default function Home() {
                 <span className='px-3 py-1 pl-0'>Kotlin</span>
                 <span className='px-3 py-1'>Java</span>
                 <span className='px-3 py-1'>Spring Boot</span>
+                <span className='px-3 py-1'>JUnit</span>
                 <span className='px-3 py-1 pr-0'>Jooq</span>
               </div>
             </>
@@ -64,76 +65,12 @@ export default function Home() {
                   Várias atividades, como, reuniões dos membros, checagem da lista de ocupantes e planejamento eram ativides custosas, pois
                   eram feitas buscas em fichas de registro de papel.
                 </p>
-                <p>Foi o meu primeiro projeto a usar Continuous Devlivery & Continuous Deployment usando Github Actions.</p>
               </div>
               <div className='flex py-4'>
                 <span className='px-3 py-1 pl-0'>CI</span>
                 <span className='px-3 py-1'>CD</span>
                 <span className='px-3 py-1'>Firebase</span>
-                <span className='px-3 py-1'>Next.js</span>
-                <span className='px-3 py-1 pr-0'>React-Query</span>
-              </div>
-            </>
-          }
-        />
-
-        <Project
-          name='Telzir'
-          img='/telzirWeb.png'
-          href='https://telzirx.netlify.app'
-          link='https://telzirx.netlify.app'
-          githubLink='https://github.com/hexetia/telzir'
-          externalHref
-          description={
-            <>
-              <div className='text-base'>
-                <p>Design e implementação de uma landing page responsiva, mobile-first de uma operadora de telefonia fictícia.</p>
-              </div>
-              <div className='flex py-4'>
-                <span className='px-3 py-1 pl-0'>React</span>
-                <span className='px-3 py-1'>Typescript</span>
-              </div>
-            </>
-          }
-        />
-
-        <Project
-          name='React-Fitty'
-          img='/react-fitty.png'
-          href='https://github.com/hexetia/react-fitty'
-          link='https://github.com/hexetia/react-fitty'
-          externalHref
-          description={
-            <>
-              <div className='text-base'>
-                <p>Componente em react que escala textos ou frases para ocupar toda a largura do componente pai.</p>
-              </div>
-              <div className='flex py-4'>
-                <span className='px-3 py-1 pl-0'>Component</span>
-                <span className='px-3 py-1'>React</span>
-              </div>
-            </>
-          }
-        />
-
-        <Project
-          name='useClientRouter para Next.js'
-          img='/use-client-router.png'
-          externalHref
-          href='https://github.com/hexetia/use-client-router'
-          link='https://github.com/hexetia/use-client-router'
-          description={
-            <>
-              <div className='text-base'>
-                <p>
-                  React hook que melhora o router do next.js no frontend, basta subistituir no seu componente o router padrão do next.js e
-                  voilà, você ganhou um router melhorado.
-                </p>
-              </div>
-              <div className='flex py-4'>
-                <span className='px-3 py-1 pl-0'>Component</span>
-                <span className='px-3 py-1'>React</span>
-                <span className='px-3 py-1'>Next.js</span>
+                <span className='px-3 py-1'>Node.js</span>
               </div>
             </>
           }
